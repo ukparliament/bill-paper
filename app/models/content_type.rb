2c765100d2bb356a4b532a604c81686b8c70dda2
fantasticlife <michael@smethur.st:wq>
2023-03-09 18:45:14 +0000

    removed display label method

--- app/models/content_type.rb
+++ app/models/content_type.rb
@@ -1,21 +1,2 @@
 class ContentType < ApplicationRecord
-  
-  def display_label
-    display_label = ''
-    case self.content_type
-      when 'application/pdf'
-        display_label = 'PDF'
-      when 'text/html' || 'application/xhtml+xml'
-        display_label = 'HTML'
-      when 'application/octet-stream'
-        display_label = 'Binary'
-      when 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' || 'application/msword'
-        display_label = 'MS Word'
-      when 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        display_label = 'MS Excel'
-      when 'image/tiff'
-        display_label = 'TIFF image'
-      end
-    display_label = '[' + display_label + ']'
-  end
 end
