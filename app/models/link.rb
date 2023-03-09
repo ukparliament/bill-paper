7625f2f3eac19e55aac66550a970f262589cc8d3
fantasticlife <michael@smethur.st:wq>
2023-03-09 18:44:23 +0000

    new method to get links

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,4 +12,23 @@ class Link < ApplicationRecord
     description += " #{self.content_type.display_label}"
     description
   end
+  
+  def content_type_display_label
+    display_label = ''
+    case self.content_type_content_type
+      when 'application/pdf'
+        display_label = 'PDF'
+      when 'text/html' || 'application/xhtml+xml'
+        display_label = 'HTML'
+      when 'application/octet-stream'
+        display_label = 'Binary'
+      when 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' || 'application/msword'
+        display_label = 'MS Word'
+      when 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        display_label = 'MS Excel'
+      when 'image/tiff'
+        display_label = 'TIFF image'
+      end
+    display_label = '[' + display_label + ']'
+  end
 end
