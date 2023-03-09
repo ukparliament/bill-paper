d58a5753f434c9da435b3900323365bbe8c453ec
fantasticlife <michael@smethur.st:wq>
2023-03-09 12:20:45 +0000

    added description used in rss

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,10 @@ class Link < ApplicationRecord
   
   belongs_to :publication
   belongs_to :content_type
+  
+  def description
+    description = "#{self.publication.publication_type.label} - for the #{self.publication.bill.short_title} published on #{self.publication.display_date}"
+    description += " #{self.content_type.display_label}"
+    description
+  end
 end
--- app/models/publication_file.rb
+++ app/models/publication_file.rb
@@ -6,4 +6,10 @@ class PublicationFile < ApplicationRecord
   def url
     "https://bills.parliament.uk/publications/#{self.publication.bill_system_id}/documents/#{self.bill_system_id}"
   end
+  
+  def description
+    description = "#{self.publication.publication_type.label} - for the #{self.publication.bill.short_title} published on #{self.publication.display_date}"
+    description += " #{self.content_type.display_label}"
+    description
+  end
 end
