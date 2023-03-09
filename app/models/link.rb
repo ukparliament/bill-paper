f49be52dbee3e109040c49e85c94bcb187d6af69
fantasticlife <michael@smethur.st:wq>
2023-03-09 14:10:29 +0000

    added display title method

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,10 @@ class Link < ApplicationRecord
   
   belongs_to :publication
   belongs_to :content_type
+
+  def display_title
+    self.title || 'Untitled'
+  end
   
   def description
     description = "#{self.publication.publication_type.label} - for the #{self.publication.bill.short_title} published on #{self.publication.display_date}"
