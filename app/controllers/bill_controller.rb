29f890b5de717159a8259458977c0954a9f340b0
fantasticlife <michael@smethur.st:wq>
2023-08-08 16:56:36 +0100

    added urls for rss and csv. set an alternate title to use in rel-alternate

--- app/controllers/bill_controller.rb
+++ app/controllers/bill_controller.rb
@@ -9,5 +9,8 @@ class BillController < ApplicationController
     bill = params[:bill]
     @bill = Bill.find_by_bill_system_id( bill )
     @page_title = @bill.short_title
+    @alternate_title = "#{@page_title = @bill.short_title} - Papers"
+    @rss_url = bill_show_url( :format => 'rss' )
+    @csv_url = bill_show_url( :format => 'csv' )
   end
 end
--- app/controllers/publication_type_controller.rb
+++ app/controllers/publication_type_controller.rb
@@ -3,11 +3,16 @@ class PublicationTypeController < ApplicationController
   def index
     @publication_types = PublicationType.all.order( 'label' )
     @page_title = 'Publication types'
+    @alternate_title = 'Publication types'
+    @csv_url = publication_type_list_url( :format => 'csv' )
   end
   
   def show
     publication_type = params[:publication_type]
     @publication_type = PublicationType.find_by_bill_system_id( publication_type )
     @page_title = @publication_type.label
+    @alternate_title = @publication_type.label
+    @rss_url = publication_type_show_url( :format => 'rss' )
+    @csv_url = publication_type_show_url( :format => 'csv' )
   end
 end
