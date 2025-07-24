04ebfab980afe4acc87d0c1d2f436a68cfa9bc0a
James Jefferies <james@shedcode.co.uk>
2025-07-24 22:20:05 +0100

    And add asset host config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,6 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+config.asset_host = ENV.fetch("ASSET_HOST", "https://bill-papers.herokuapp.com/")
+
 end
