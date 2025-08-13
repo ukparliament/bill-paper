72ce6b40ca970bab04f418380b5512e648dc92ac
fantasticlife <michael@smethur.st>
2025-08-13 19:48:21 +0100

    added page meta stuff to controller actions

--- app/controllers/bill_category_controller.rb
+++ app/controllers/bill_category_controller.rb
@@ -2,16 +2,18 @@ class BillCategoryController < ApplicationController
   def index
     @bill_categories = BillCategory.all.order( 'label' )
     @page_title = 'Bill categories'
-
+    @description = 'Bill categories.'
     @crumb << { label: @page_title, url: nil }
+    @section = 'categories'
   end
   
   def show
     bill_category = params[:bill_category]
     @bill_category = BillCategory.find( bill_category )
     @page_title = "#{@bill_category.label} bills"
-
+    @description = "#{@bill_category.label} bills."
     @crumb << { label: 'Bill categories', url: bill_category_list_url }
     @crumb << { label: @page_title, url: nil }
+    @section = 'categories'
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @description = 'An application to provide RSS feeds for papers associated with bills before the UK Parliament.'
   end
 end
