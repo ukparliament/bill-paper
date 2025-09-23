4bb5421961f9c2292c97cdb0fc652300adfbfd2b
fantasticlife <michael@smethur.st>
2025-09-23 23:09:35 +0100

    added setting of portcullis toggle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   include LibraryDesign::Crumbs
 
   $SITE_TITLE = 'Bill Papers'
+  
+  $TOGGLE_PORTCULLIS = ENV.fetch( "TOGGLE_PORTCULLIS", 'off' )
 end
