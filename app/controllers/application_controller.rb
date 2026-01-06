1a58b34556ed4fcc2c1a0a31d291821765bcb6cb
fantasticlife <michael@smethur.st>
2026-01-06 10:36:47 +0000

    added csv title method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   $SITE_TITLE = 'Bill Papers'
   
   $TOGGLE_PORTCULLIS = ENV.fetch( "TOGGLE_PORTCULLIS", 'off' )
+
+  private
+  
+  def csv_title_from_page_title( page_title )
+    page_title.downcase.gsub( ' ', '-' ).gsub( '---', '-' )
+  end
 end
